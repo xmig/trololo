12b3a16e143110591750ba0198f83321e9b1ed49
Yuriy Z <tretyak@gmail.com>
2016-02-29 18:14:02 +0200

    Add fconfigs for build and deploy app

---
+++ app_run/stage/StageRun.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "++++++ Start Staging Project Script ++++++"
+
+echo "------------------------------------------------------"
+echo "User:"
+whoami
+echo "------------------------------------------------------"
+
+echo "-------------------- Go to (APP home) --------------------"
+cd /home/ubuntu/projects/trololo/
+pwd
+echo "------------------------------------------------------"
+
+echo "-== Activate Venv ==-"
+source /home/ubuntu/projects/trololo/venv/bin/activate
+echo "------------------------------------------------------"
+
+echo "-== Install python Packeges ==-"
+pip install -r requirements.txt
+sleep 1
+
+#echo "#----------------------------------- Collection Static: NUCLEUS -----------------------------------#"
+#cd /home/nucleus/www/Staging/nucleus/NUCLEUS
+#./manage.py collectstatic --noinput
+#echo "#-------------------------------- Collection Static: NUCLEUS: END --------------------------------#"
+
+#echo "#----------------------------------- Collection Static: WEBSHOP -----------------------------------#"
+#cd /home/nucleus/www/Staging/fragrantjewels/WEBSHOP
+#./manage.py collectstatic --noinput
+#echo "#-------------------------------- Collection Static: WEBSHOP: END --------------------------------#"
+
+echo "#----------------------------------- END -----------------------------------#"
