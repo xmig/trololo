116f4355bd489bdb660b43eb57caf5b86eeb0a23
Max Novak <maxim.novak@chisw.com>
2016-03-10 16:38:19 +0200

    Fixed authentication

--- trololo/trololo/settings/base.py
+++ trololo/trololo/settings/base.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
     )
