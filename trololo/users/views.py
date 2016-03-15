b61ddb64add12cf578b00120e14f88e287f2dd4b
Max Novak <maxim.novak@chisw.com>
2016-03-15 12:54:16 +0200

    Resolve conflicts

--- trololo/users/views.py
+++ trololo/users/views.py
@@ -1,95 +1,85 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import get_user_model
-from django.contrib.auth.decorators import login_required
 from users.serializers import UserSerializer
-from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.generics import GenericAPIView
 from rest_framework import status
 import requests
-from django.conf import settings
 from django.views.generic import TemplateView
-
-
 from django.core.urlresolvers import reverse
+# from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+# from rest_framework.authentication import SessionAuthentication
+# from rest_framework import permissions
+# from django.contrib.auth import get_user_model
 
 
-def app_login(request):
-    if request.method == 'POST':
-        user = request.POST['username']
-        passwd = request.POST['password']
-
-        user = authenticate(username=user, password=passwd)
-
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-
-                return redirect('/users/user/')
-
-        else:
-            return render(request, 'login.html')
-    else:
-        return render(request, 'login.html')
-
-
-@login_required
-def user_list(request):
-    return render(request, 'users.html', {'users_list': get_user_model().objects.all()})
+# class UserList(APIView):
+#     # authentication_classes = (SessionAuthentication,)
+#     permission_classes = (permissions.IsAuthenticated,)
+#
+#     def get(self, request):
+#         User = get_user_model()
+#
+#         return Response(
+#             UserSerializer(User.objects.all(), many=True).data
+#         )
+#
+#     def post(self, request):
+#         s = UserSerializer(data=request.data)
+#         if s.is_valid():
+#             s.save()
+#
+#             return Response(s.data, status=status.HTTP_201_CREATED)
+#
+#         return Response({"errors": s.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UserList(APIView):
-    # queryset = User.objects.all()
-    # serializer_class = UserSerializer
+# class CsrfExemptSessionAuthentication(SessionAuthentication):
+#
+#     def enforce_csrf(self, request):
+#         return  # To not perform the csrf check previously happening
 
-    # authentication_classes = (SessionAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
+
+# class SingleUser(APIView):
+#     # authentication_classes = (SessionAuthentication,)
+#     permission_classes = (permissions.IsAuthenticated,)
+#
+#     def get(self, request, id):
+#         try:
+#             user = get_user_model().objects.get(pk=id)
+#         except get_user_model().DoesNotExist:
+#             return Response({}, status=status.HTTP_404_NOT_FOUND)
+#
+#         return Response(UserSerializer(user).data)
+#
+#     def put(self, request, id):
+#         s = UserSerializer(data=request.data, photo=request.data['file'])
+#         if s.is_valid():
+#             s.save()
+#
+#             return Response(s.data, status=status.HTTP_200_OK)
+#
+#         return Response({"errors": s.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfile(GenericAPIView):
+    """
+        Get/Update current logged in user profile data.
+    """
+    serializer_class = UserSerializer
+    # parser_classes = (MultiPartParser, FormParser, JSONParser)
 
     def get(self, request):
-        User = get_user_model()
-        print(User)
+        u = self.get_serializer_class()(request.user)
 
-        return Response(
-            UserSerializer(User.objects.all(), many=True).data
-        )
+        return Response(u.data)
 
-    def post(self, request):
-        s = UserSerializer(data=request.data)
+    def put(self,request):
+        s = self.get_serializer_class()(request.user, data=request.data)
+
         if s.is_valid():
             s.save()
 
             return Response(s.data, status=status.HTTP_201_CREATED)
-
-        return Response({"errors": s.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CsrfExemptSessionAuthentication(SessionAuthentication):
-
-    def enforce_csrf(self, request):
-        return  # To not perform the csrf check previously happening
-
-
-class SingleUser(APIView):
-    # authentication_classes = (SessionAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get(self, request, id):
-        try:
-            user = get_user_model().objects.get(pk=id)
-        except get_user_model().DoesNotExist:
-            return Response({}, status=status.HTTP_404_NOT_FOUND)
-
-        return Response(UserSerializer(user).data)
-
-    def put(self, request, id):
-        s = UserSerializer(data=request.data)
-        if s.is_valid():
-            s.save()
-
-            return Response(s.data, status=status.HTTP_200_OK)
-
         return Response({"errors": s.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -112,3 +102,6 @@ class AccountConfirmEmailView(APIView):
 
 class MainView(TemplateView):
     template_name = 'index.html'
+
+
+
