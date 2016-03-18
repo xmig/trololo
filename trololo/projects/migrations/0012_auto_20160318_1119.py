7ec08de58c7de4ecb1973b27a752332f29fed1c5
Julia <julswork@gmail.com>
2016-03-18 17:51:45 +0200

    Merge branch 'stage' into project_tests
    Conflicts:
    	trololo/users/urls.py

--- trololo/projects/migrations/0012_auto_20160318_1119.py
+++ trololo/projects/migrations/0012_auto_20160318_1119.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0011_auto_20160314_1619'),
+        ('projects', '0002_project_activity'),
     ]
 
     operations = [
--- trololo/users/urls.py
+++ trololo/users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^profile/$', views.UserProfile.as_view(), name='user_profile'),
 
 
-    url(r'^(?P<pk>[0-9]+)/$', views.SingleUser.as_view(), name='users_list'),
-    url(r'^users_list/$', views.UsersList.as_view(), name='users_list'),
+    # url(r'^(?P<pk>[0-9]+)/$', views.SingleUser.as_view(), name='users_list'),
+    # url(r'^users_list/$', views.UsersList.as_view(), name='users_list'),
     url('^(?P<id>\d+)/$', views.SingleUser.as_view(), name='single_user')
 ]
