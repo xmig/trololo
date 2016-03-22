f105d5387179120cc0c7bd8ea1a7f7a445358a58
Yuriy Z <tretyak@gmail.com>
2016-03-22 15:31:57 +0200

    Add script to install postgresql

--- app_run/installation.sh
+++ app_run/installation.sh
@@ -1,6 +1,20 @@
-# clone project
-git clone https://github.com/xmig/trololo trololo_dev
-cd trololo_dev/
+#!/bin/bash
+set -ex
+# RUN this script as superuser from PROJECT ROOT
+if [ "${UID}" != 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+if [ -f "$PWD/README.md" ]; then
+    if [ -z "$(cat README.md | grep 'Task/Issue/Bug tracker')" ]; then
+        echo "This script must be run from the project root"
+        exit 1
+    fi
+else
+    echo "This script must be run from the project root"
+    exit 1
+fi
 
 # install required packages
 sudo apt-get install python-dev python-pip postgresql-9.3 postgresql-server-dev-9.3
@@ -12,17 +26,18 @@ sudo service postgresql reload
 # add trololo/ dir to the PYTHONPATH
 export PYTHONPATH="${PWD}/trololo/"
 
-# LOCAL_DB_USER="test"
-# LOCAL_DB_NAME="test"
-# LOCAL_DB_PASSWORD="test"
-
+# get user, DB name and password from config
 LOCAL_DB_USER=$(python -c "from trololo import settings; print settings.DATABASES['default']['USER']")
 LOCAL_DB_NAME=$(python -c "from trololo import settings; print settings.DATABASES['default']['NAME']")
 LOCAL_DB_PASSWORD=$(python -c "from trololo import settings; print settings.DATABASES['default']['PASSWORD']")
 
+echo $LOCAL_DB_USER
+echo $LOCAL_DB_NAME
+echo $LOCAL_DB_PASSWORD
+
 # create role and database
 sudo psql -U postgres -c "CREATE ROLE ${LOCAL_DB_USER} LOGIN;"
-sudo psql -U postgres -c "ALTER ROLE ${LOCAL_DB_USER} WITH PASSWORD ''${LOCAL_DB_PASSWORD}';"
+sudo psql -U postgres -c "ALTER ROLE ${LOCAL_DB_USER} WITH PASSWORD '${LOCAL_DB_PASSWORD}';"
 sudo psql -U postgres -c "CREATE DATABASE ${LOCAL_DB_NAME} OWNER ${LOCAL_DB_USER};"
 
 sudo service postgresql restart
@@ -31,15 +46,3 @@ sudo service postgresql restart
 sudo echo "local   all             ${LOCAL_DB_USER}                                md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 sudo sed -i 's/local   all             postgres                                trust/local   all             postgres                                peer/g' /etc/postgresql/9.3/main/pg_hba.conf
 sudo service postgresql restart
-
-# create virtual env
-pip install virtualenv
-
-virtualenv env
-
-source env/bin/activate
-
-# install requirements
-pip install -r test_requirements.txt
-deactivate
-
