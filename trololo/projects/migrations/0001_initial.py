dd1d52a16bb0ee041d45f31e31399220e6f1f62c
Yuriy Z <tretyak@gmail.com>
2016-03-24 09:48:05 +0200

    Created new migrations

--- trololo/projects/migrations/0001_initial.py
+++ trololo/projects/migrations/0001_initial.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
+import chi_django_base.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('activity', '0001_initial'),
     ]
 
     operations = [
@@ -16,18 +16,19 @@ class Migration(migrations.Migration):
             name='Project',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
                 ('name', models.CharField(default='', max_length=100, null=True, blank=True)),
                 ('status', models.CharField(default='undefined', max_length=30, choices=[('breakthrough', 'Breakthrough'), ('in_progress', 'In_progress'), ('finished', 'Finished'), ('undefined', 'Undefined')])),
                 ('description', models.TextField(default='', max_length=1000, null=True, blank=True)),
                 ('visible_by', models.CharField(default='undefined', max_length=30, choices=[('members', 'Members'), ('particular_user', 'Particular_user'), ('all_users', 'All_users'), ('undefined', 'Undefined')])),
                 ('date_started', models.DateTimeField(default='', null=True, blank=True)),
                 ('date_finished', models.DateTimeField(default='', null=True, blank=True)),
-                ('member', models.ManyToManyField(related_name='projects_added', null=True, to=settings.AUTH_USER_MODEL, blank=True)),
-                ('owner', models.ForeignKey(related_name='projects_owned', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
             ],
             options={
                 'abstract': False,
             },
+            bases=(models.Model, chi_django_base.models.AbstractAddOldObject),
         ),
         migrations.CreateModel(
             name='ProjectComment',
@@ -36,7 +37,6 @@ class Migration(migrations.Migration):
                 ('comment', models.TextField(default='', null=True, blank=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('modified_at', models.DateTimeField(auto_now=True)),
-                ('project', models.ForeignKey(default='', blank=True, to='projects.Project', null=True)),
             ],
             options={
                 'abstract': False,
@@ -46,21 +46,22 @@ class Migration(migrations.Migration):
             name='Task',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
                 ('status', models.CharField(default='undefined', help_text='choose status', max_length=30, choices=[('breakthrough', 'Breakthrough'), ('in_progress', 'In_progress'), ('finished', 'Finished'), ('undefined', 'Undefined')])),
                 ('type', models.CharField(default='undefined', help_text='choose type', max_length=30, choices=[('bug', 'Bug'), ('feature', 'Feature'), ('undefined', 'Undefined')])),
-                ('label', models.CharField(default='undefined', help_text='choose label', max_length=50, choices=[('red', 'Breakthrough'), ('orange', 'In_progress'), ('green', 'Finished'), ('undefined', 'Undefined')])),
+                ('label', models.CharField(default='undefined', help_text='choose label', max_length=50, choices=[('red', 'Red'), ('orange', 'Orange'), ('green', 'Green'), ('undefined', 'Undefined')])),
                 ('name', models.CharField(default='', max_length=150, null=True, blank=True)),
                 ('description', models.TextField(default='', null=True, blank=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('modified_at', models.DateTimeField(auto_now=True)),
                 ('deadline_date', models.DateTimeField(default='', null=True, blank=True)),
                 ('estimate_minutes', models.IntegerField(default='', null=True, blank=True)),
-                ('member', models.ManyToManyField(related_name='tasks_added', null=True, to=settings.AUTH_USER_MODEL, blank=True)),
-                ('project', models.ForeignKey(default='', blank=True, to='projects.Project', null=True)),
+                ('activity', models.ManyToManyField(related_name='task_activities', to='activity.Activity', blank=True)),
             ],
             options={
                 'abstract': False,
             },
+            bases=(models.Model, chi_django_base.models.AbstractAddOldObject),
         ),
         migrations.CreateModel(
             name='TaskComment',
