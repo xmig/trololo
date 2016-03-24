e2eae5fa3e50f10c4486d838fcf23fa2261b46be
Juls <julswork@gmail.com>
2016-03-24 16:24:28 +0200

    modified project|task serializers to HyperlinkedModelSerializer subclasses

--- trololo/projects/urls.py
+++ trololo/projects/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^projects/$', views.ProjectsList.as_view(), name='projects'),
     url(r'^projects/(?P<pk>[0-9]+)/$', views.ProjectDetail.as_view(), name='projects_detail'),
     url(r'^tasks/$', views.TaskList.as_view(), name='tasks'),
-    url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view(), name='task_detail'),
+    url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view(), name='tasks_detail'),
 ]
