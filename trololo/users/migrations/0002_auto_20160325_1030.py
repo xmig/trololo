8b2ba184eb1fefc110bd810e0d9c8e89050f5e6d
Yuriy Z <tretyak@gmail.com>
2016-03-25 15:26:07 +0200

    Updated migration

--- trololo/users/migrations/0002_auto_20160325_1030.py
+++ trololo/users/migrations/0002_auto_20160325_1030.py
@@ -12,7 +12,8 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL([
-            "UPDATE django_site SET domain='worddict.net:81', name='worddict.net:81' WHERE id=1;",
+            "DELETE FROM django_site;",
+            "INSERT INTO django_site (id, domain, name) VALUES (1, 'worddict.net:81', 'worddict.net:81');",
             "INSERT INTO django_site (id, domain, name) VALUES (2, '127.0.0.1:8000', '127.0.0.1:8000');",
             "INSERT INTO django_site (id, domain, name) VALUES (3, 'worddict.net:82', 'worddict.net:82');",
             "ALTER SEQUENCE django_site_id_seq RESTART WITH 4;",
