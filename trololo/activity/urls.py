8a803e704031e118f3f239f8631854e0b35494c2
dmitrij <dimapaschenko@gmail.com>
2016-03-31 18:01:40 +0300

    activity API for all activity

--- trololo/activity/urls.py
+++ trololo/activity/urls.py
@@ -3,5 +3,10 @@ from activity import views
 
 
 urlpatterns = [
+    # <type>
+    # a - all activity
+    # p - project activity only
+    # t - task activity only
+    url('^(?P<show_type>[apt])/$', views.Activities.as_view(), name='activities'),
     url('^(?P<id>\d+)/$', views.SingleActivity.as_view(), name='single_activity')
 ]
--- trololo/activity/views.py
+++ trololo/activity/views.py
@@ -1,9 +1,11 @@
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework import status
+from django.db.models import Q
+from rest_framework import filters
 from activity.serializers import ActivitySerializer
 from activity.models import Activity
-from projects.models import Project
-from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
-from rest_framework import status
+from activity.filters import ActivityFilter
 
 class SingleActivity(GenericAPIView):
     queryset = Activity.objects.all()
@@ -24,3 +26,64 @@ class SingleActivity(GenericAPIView):
 
         return response
 
+
+class Activities(ListAPIView):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter,)
+    filter_class = ActivityFilter
+    ordering_fields = ('message', 'created_at',)
+    ordering = ('-created_at',)
+
+    def get(self, request, show_type):
+        """
+        Get activities data by show_type \n
+        {show_type}  : \n
+        a  -  get all activity \n
+        p  - get only project activity \n
+        t  - get only task activity \n\n
+
+        Available filters:\n
+        for_cu        - get data filtered by current user\n
+        message       - filter by strict activity message\n
+        message_like  - filter by contains activity message (insensitive)\n
+        date_0        - from date (created_at)\n
+        date_1        - to date (created_at)\n\n
+
+        Available sorting (send param like this ?sorting=filter1,filter2,.....):\n
+
+        message\n
+        -message\n
+        created_at\n
+        -created_at\n
+
+        """
+        try:
+            for_current_user=request.GET.get('for_cu', False)
+            self.queryset = self.filter_queryset(self.get_queryset())
+
+            if show_type == 'a':
+                # all activity
+                activity_type_query = Q(project_activities__gt=0) | Q(task_activities__gt=0)
+            elif show_type == 'p':
+                # project only activity
+                activity_type_query = Q(project_activities__gt=0)
+            elif show_type == 't':
+                # task only activity
+                activity_type_query = Q(task_activities__gt=0)
+            else:
+                activity_type_query = Q(project_activities__gt=0) | Q(task_activities__gt=0)
+
+            activities = self.get_queryset().filter(activity_type_query)
+
+            if for_current_user:
+                activities = activities.filter(created_by=int(request.user.id))
+
+            data = ActivitySerializer(activities, many=True).data
+            response = Response(data)
+        except Activity.DoesNotExist:
+            response = Response({}, status=status.HTTP_404_NOT_FOUND)
+        except:
+            response = Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return response
