9c77fcbad123537a9672667c04a2f7cab6cc9d01
dimapaschenko <dimapaschenko@gmail.com>
2016-04-12 01:29:01 -0700

    revert local setting

--- trololo/trololo/settings/local_settings.py
+++ trololo/trololo/settings/local_settings.py
@@ -3,13 +3,13 @@ EMAIL_HOST_PASSWORD = 'QueaK1chu3io'
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = 'jonny.john2017@yandex.ru'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test_db',
-        'USER': 'test_user',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'test_db',
+#         'USER': 'test_user',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
