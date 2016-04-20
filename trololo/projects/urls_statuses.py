077c22596587ac098ec41d83ecb29820bc82ba27
Sergey Tretyak <tretyak@gmail.com>
2016-04-20 15:00:39 +0300

    add group/status filter for tasks

--- trololo/projects/urls_statuses.py
+++ trololo/projects/urls_statuses.py
@@ -4,6 +4,6 @@ from projects.views_status import StatusDetail, StatusView
 
 
 urlpatterns = [
-    url(r'^status/$', StatusView.as_view(), name='status'),
-    url(r'^status/(?P<pk>[0-9]+)/$', StatusDetail.as_view(), name='status_detail')
+    url(r'^$', StatusView.as_view(), name='status'),
+    url(r'^(?P<pk>[0-9]+)/$', StatusDetail.as_view(), name='status_detail')
 ]
