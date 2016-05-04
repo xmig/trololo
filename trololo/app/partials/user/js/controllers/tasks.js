d96c22b0c3a18796fc131db892098ca7879f1430
Juls <julswork@gmail.com>
2016-05-04 12:14:35 +0300

    f

--- trololo/app/partials/user/js/controllers/tasks.js
+++ trololo/app/partials/user/js/controllers/tasks.js
@@ -1,13 +1,31 @@
 angular.module('userApp').controller('tasksCtrl', ['$scope', '$rootScope', '$http', 'taskService', 'projectService', 'activityListService',  '$mdDialog', '$mdMedia',
                                            function($scope, $rootScope, $http, taskService, projectService, activityListService, $mdDialog, $mdMedia){
 
-//alert();
-//console.log(data)
+////alert();
+////console.log(data)
+//        $scope.tasks = {}
+//        $scope.tasks.data = data.results;
+////        console.log(tasks.data)
+//        $scope.tasks.count = $scope.tasks.data.length;
+//        console.log('data!!', $scope.tasks.data);
+//
+//
+//// for dropdown in popup <choose projects>
+//    $scope.projects = projectService.get(function(data) {
+////        $scope.projects.data = data.results;
+//        console.log("---------------", data)
+//    })
+//
+
+
+
+    taskService.get({page_size: 1000}, function (data) {  //parameter {page_size: 1000} for display pages in table (10 items by default)
         $scope.tasks = {}
         $scope.tasks.data = data.results;
-//        console.log(tasks.data)
         $scope.tasks.count = $scope.tasks.data.length;
-        console.log('data!!', $scope.tasks.data);
+        console.log('data!!', $scope.tasks.data)
+
+    });
 
 
 // for dropdown in popup <choose projects>
@@ -15,7 +33,6 @@ angular.module('userApp').controller('tasksCtrl', ['$scope', '$rootScope', '$htt
 //        $scope.projects.data = data.results;
         console.log("---------------", data)
     })
-    
 
 
 
