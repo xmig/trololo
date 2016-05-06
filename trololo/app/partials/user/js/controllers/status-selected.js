1b13268bcdc403ba404a31865e5bc14980f60d5b
Yuriy Z <yuriy.zadneprovskiy@chisw.com>
2016-05-06 15:35:07 +0300

    Add top menu & refresh button in projects list.

--- trololo/app/partials/user/js/controllers/status-selected.js
+++ trololo/app/partials/user/js/controllers/status-selected.js
@@ -67,15 +67,6 @@ angular.module('userApp').controller('statusSelectedCtrl', ['$scope', '$rootScop
         }
     );
 
-    $scope.statusSaveToast = function(text) {
-        $mdToast.show(
-            $mdToast.simple()
-                .textContent(text)
-                .position("top right")
-                .hideDelay(3000)
-        );
-    };
-
     $scope.saveStatus = function() {
         if ($scope.statusDataCopy.name !== $scope.statusData.name || $scope.statusDataCopy.order_number !== $scope.statusData.order_number) {
             projectSelectedStatusService.put(
