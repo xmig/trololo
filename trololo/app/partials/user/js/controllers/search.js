bb5a419ace92b05f730e576b0b77cff0d388a1fe
Yuriy Z <yuriy.zadneprovskiy@chisw.com>
2016-05-11 15:47:34 +0300

    Add search in frontend.

--- trololo/app/partials/user/js/controllers/search.js
+++ trololo/app/partials/user/js/controllers/search.js
@@ -1,17 +1,20 @@
 angular.module('userApp').controller('searchCtrl', ['$scope', '$mdDialog', '$mdMedia', '$rootScope', 'searchService',
     function($scope, $mdDialog, $mdMedia, $rootScope, searchService){
 
-    $scope.search_results = {
-        project: [],
-        task: [],
-        task_comment: []
-    };
+    $scope.project = [];
+    $scope.task = [];
+    $scope.task_comment = [];
+
+    $scope.search_text = $rootScope.searchPhrase;
 
     if ($rootScope.searchPhrase) {
         searchService.get(
             {search_phrase: $rootScope.searchPhrase},
             function(data) {
-                $scope.search_results = data;
+                $scope.project = data.project;
+                $scope.task = data.task;
+                $scope.task_comment = data.task_comment;
+
             },
             function(resp) {
                 console.log("RESPONSE: ", resp);
